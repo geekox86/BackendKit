98b25e5e3c7661d8deae566d7037dd69a6f3c5cc
Mohannad Awad <geekox86@gmail.com>
2025-03-18 05:31:14 +0000

    Replacing Xunit with TUnit

--- src/Interfacing/InterfacingConfiguration.test.cs
+++ src/Interfacing/InterfacingConfiguration.test.cs
@@ -1,19 +1,19 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.TestHost;
-using Xunit;
+
+// using Microsoft.AspNetCore.TestHost;
 
 namespace BackendKitTests;
 
 public class InterfacingConfigurationTests
 {
-  [Fact]
-  public void Test()
+  [Test]
+  public async Task Test()
   {
     var appBuilder = WebApplication.CreateBuilder();
     var app = appBuilder.Build();
-    using var appServer = app.GetTestServer();
-    using var appClient = app.GetTestClient();
+    // using var appServer = app.GetTestServer();
+    // using var appClient = app.GetTestClient();
 
-    Assert.True(true);
+    await Assert.That(true).IsTrue();
   }
 }
